23615c77583f99840446176024b707341516e176
gabinwilliams <scoutsnknives@gmail.com>
2021-03-19 16:11:19 -0500

    GET route completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,5 +8,26 @@ function handleReady() {
 
   console.log('jQuery ready!');
 
-
+  getTasks();
 }// end handleReady
+
+
+function getTasks() {
+
+  $.ajax({
+    type: 'GET',
+    url: '/toDo'
+
+  }).then(function (response) {
+    response.forEach((task) => {
+      // let completeStatus = task.complete === true ? 'Yes' : 'No';
+      $('#displayTask').append(`
+          <li data-status="${task.complete}" >${task.task} 
+          <button data-id="${task.id}" class="deleteBtn">Delete</button>
+          </li>
+        
+      `);
+    });
+  });
+} // end getTasks
+
--- server/routes/toDo.router.js
+++ server/routes/toDo.router.js
@@ -5,7 +5,26 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 
+// GET ROUTE
 
+router.get('/', (req, res) => {
+
+  let queryText = `
+  
+    SELECT * FROM "todo" ORDER BY "id";
+  `
+  pool.query(queryText).then((result) => {
+    console.log('In GET sending:', result);
+
+    res.send(result.rows);
+  }).catch((error) => {
+
+    console.log('error in GET', error);
+
+    res.sendStatus(500);
+
+  });
+});// end GET
 
 
 
